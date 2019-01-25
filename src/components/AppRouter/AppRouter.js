9087b96f4af60cd3eb4acd392f6357fd7d63e32e
Kosta <satkon@gmail.com>
2019-01-25 10:32:53 +1000

    Add Navigation

--- src/components/AppRouter/AppRouter.js
+++ src/components/AppRouter/AppRouter.js
@@ -10,17 +10,53 @@
 // Так же в этом файле обьявите лейаут,
 // используйте стили из AppRouter.module.css
 import React, { PureComponent } from 'react';
+import { Route, NavLink } from 'react-router-dom';
 import styles from './AppRouter.module.css';
+import Home from '../Home';
+import InboxList from '../InboxList';
+import OutboxList from '../OutboxList';
 
 class AppRouter extends PureComponent {
-    render() {
-      return (
-        <div className={styles.wrapper}>
-          Hello
+  render() {
+    const { match } = this.props;
+    return (
+      <div className={styles.wrapper}>
+        <div className={styles.container}>
+          <nav className={styles.nav}>
+            <ul className={`${styles.navList} t-nav-list`}>
+              <li className={styles.navElement}>
+                <NavLink to="/app" title="Home" className={`${styles.link} t-link-home`}>
+                  Home
+                </NavLink>
+              </li>
+              <li className={styles.navElement}>
+                <NavLink 
+                  to={`${match.url}/inbox`}
+                  className={`${styles.link} t-link-inbox`}
+                >
+                  Inbox
+                </NavLink>
+              </li>
+              <li className={styles.navElement}>
+                <NavLink
+                  to={`${match.url}/outbox`}
+                  className={`${styles.link} t-link-outbox`}
+                >
+                  Outbox
+                </NavLink>
+              </li>
+            </ul>
+          </nav>
+          <div className={styles.content}>
+            <h3 className={styles.title}>{match.params.title}</h3>
+            <Route path="/app" exact component={Home} />
+            <Route path={`${match.path}/inbox`} component={InboxList} />
+            <Route path={`${match.path}/outbox`} component={OutboxList} />
+          </div>          
         </div>
-      );
-    }
+      </div>
+    );
   }
-  
-  export default AppRouter;
-  
+}
+
+export default AppRouter;
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,3 +2,19 @@
 // Он должен показывать приветствие.
 // Изучите файл `/cypress/integration/homework.spec.js`, чтобы понять,
 // какие классы должен использовать компонент.
+
+import React, { PureComponent } from 'react';
+import styles from './Home.module.css';
+import { withAuth } from '../../context/Auth';
+
+class Home extends PureComponent {
+  render() {
+    return (
+      <div className={styles.container}>
+        <p className="t-greeting">Приветствуем в почтовом клиенте!</p>
+      </div>
+    );
+  }
+}
+
+export default Home;
